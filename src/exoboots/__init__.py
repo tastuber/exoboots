f2c1823b1948d8b1c182fbfbab65ca53ebf8e4bf
Thomas A. Stuber <tstuber@arizona.edu>
2025-08-25 12:55:40 -0700

    Dynamically initialize __version__ in __init.py__.

--- src/exoboots/__init__.py
+++ src/exoboots/__init__.py
@@ -1,4 +1,8 @@
+import importlib
+
 from exoboots import bootstrapping
 from exoboots import data_handling
 from exoboots import model_functions
 from exoboots import plotting
+
+__version__ = importlib.metadata.version(__name__)
